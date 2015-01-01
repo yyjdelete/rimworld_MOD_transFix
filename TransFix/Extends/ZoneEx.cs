7145faa9a459589a3d077c893fd59b37d237b599
SilverFox <yyjdelete@126.com>
2015-01-01 10:26:31 +0800

    [2.0_alpha6] 修复上一版会造成存储区丢失的问题 销毁种植当前不存在作物的种植区

--- TransFix/Extends/ZoneEx.cs
+++ TransFix/Extends/ZoneEx.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using UnityEngine;
 using Verse;
@@ -11,14 +13,34 @@ namespace TransFix.Extends
 {
     public static class ZoneEx
     {
-        
+        private static readonly Func<Zone_Growing, ThingDef> getPlantDefToGrow;
+        //private static readonly Action<Zone> callZoneExposeData;
+        static ZoneEx()
+        {
+            ParameterExpression expression;
+            getPlantDefToGrow = Expression.Lambda<Func<Zone_Growing, ThingDef>>(
+                Expression.Field(expression = Expression.Parameter(typeof(Zone_Growing), "zone"), "plantDefToGrow"),
+                new ParameterExpression[] { expression })
+                .Compile();
+
+            //Type baseType = typeof(Zone);
+            //BindingFlags flags = BindingFlags.Instance | BindingFlags.Public;
+            //var mi = baseType.GetMethod("ExposeData", flags);
+            //var methodBldr = new DynamicMethod("", null, new[] { baseType }, baseType, true);
+            //var il = methodBldr.GetILGenerator();//获取il生成器
+            //il.Emit(OpCodes.Ldarg_0);
+            //il.Emit(OpCodes.Call, mi);//Not Virt
+            //il.Emit(OpCodes.Ret);
+            //callZoneExposeData = (Action<Zone>)methodBldr.CreateDelegate(typeof(Action<Zone>));
+        }
         public static void ExposeDataEx(this Zone zone)
         {
             //zone.ExposeData();//注意是虚函数
             Scribe_Values.LookValue<string>(ref zone.zoneName, "zoneName", null, false);
-            Scribe_Values.LookValue<Color>(ref zone.zoneColor, "zoneColor", new Color(), false);
+            Scribe_Values.LookValue<Color>(ref zone.zoneColor, "zoneColor", default(Color), false);
             Scribe_Values.LookValue<bool>(ref zone.hidden, "hidden", false, false);
-            Scribe_Collections.LookList<IntVec3>(ref zone.cells, "cells", LookMode.Undefined, null);
+            Scribe_Collections.LookList<IntVec3>(ref zone.cells, "cells", LookMode.Value, null);//注意自动补全的null参数类型错误
+            //callZoneExposeData(zone);
 
             zone.FixCells();
         }
@@ -28,8 +50,8 @@ namespace TransFix.Extends
             if (zone.cells == null)
             {
                 //A7
-                Scribe_Fix.LookListNotNull<IntVec3>(ref zone.cells, "squares", LookMode.Undefined, null);
-                if (zone.cells == null)
+                Scribe_Collections.LookList<IntVec3>(ref zone.cells, "squares", LookMode.Value, null);
+                if (zone.cells == null || zone.cells.Count == 0)
                 {
                     throw new InvalidOperationException("Zone must have cells!!!");
                 }
@@ -40,11 +62,18 @@ namespace TransFix.Extends
             //((Zone)zone).ExposeDataEx();
             //Scribe_Defs.LookDef<ThingDef>(ref zone.plantDefToGrow, "plantDefToGrow");
             zone.ExposeData();
+
+            if (getPlantDefToGrow(zone) == null)
+            {
+                throw new InvalidOperationException("Unknown plant to grown");
+            }
             zone.FixCells();
         }
         public static void ExposeDataEx(this Zone_Stockpile zone)
         {
             ((Zone)zone).ExposeDataEx();
+            //callZoneExposeData(zone);
+            //zone.FixCells();
             Scribe_Fix.LookDeepNotNull<StorageSettings>(ref zone.settings, "settings", zone);
             if (Scribe.mode == LoadSaveMode.LoadingVars)
             {
